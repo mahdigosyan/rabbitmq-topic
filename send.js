a07692cc05bf3891e4ca32b756a460ff958ef0da
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-08-21 00:00:13 +0330

    Update send.js

--- send.js
+++ send.js
@@ -5,8 +5,10 @@ const sendData = async () => {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     await channel.assertExchange(exchangeName, "topic");
+    
     channel.publish(exchangeName, logType, Buffer.from(message))
     setTimeout(() => {
+
         process.exit(0)
     })
 }
